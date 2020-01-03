b9e4f8c279bd79daeffeef0f6d8484c8caf5c0cc
dejANdroid <figaroakomoze86@gmail.com>
2020-01-03 20:00:36 +0100

    Commit changes

--- app/src/main/java/com/example/comicvine/view/fragments/CharacterByIdFragment.java
+++ app/src/main/java/com/example/comicvine/view/fragments/CharacterByIdFragment.java
@@ -11,22 +11,18 @@ import android.view.ViewGroup;
 
 import com.example.comicvine.R;
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CharacterByIdFragment extends Fragment {
 
-
-    public CharacterByIdFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_by_id, container, false);
-    }
 
+        View view=inflater.inflate(R.layout.fragment_character_by_id, container, false);
+
+        return view;
+    }
 }
