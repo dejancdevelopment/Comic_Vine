c1ae2bb8bc4135ed753b973ec968f821642b45f4
dejANdroid <figaroakomoze86@gmail.com>
2020-02-22 12:44:48 +0100

    Commit changes

--- app/src/main/java/com/example/comicvine/view/adapter/adapter_issues/Main_Issues_RecyclerAdapter.java
+++ app/src/main/java/com/example/comicvine/view/adapter/adapter_issues/Main_Issues_RecyclerAdapter.java
@@ -12,7 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.example.comicvine.DetailActivity;
 import com.example.comicvine.R;
 import com.example.comicvine.data.model.model_issues.IssuesResults;
@@ -26,21 +25,23 @@ public class Main_Issues_RecyclerAdapter extends RecyclerView.Adapter<Main_Issue
 
     public Main_Issues_RecyclerAdapter(List<IssuesResults> mList, Context context) {
         this.mList = mList;
-        this.context=context;
+        this.context = context;
     }
+
     @NonNull
     @Override
     public Main_Issues_RecyclerAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_issues,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_issues, parent, false);
 
         return new MyHolder(view);
     }
+
     @Override
     public void onBindViewHolder(@NonNull Main_Issues_RecyclerAdapter.MyHolder holder, int position) {
 
-        final IssuesResults results=mList.get(holder.getAdapterPosition());
+        final IssuesResults results = mList.get(holder.getAdapterPosition());
 
-        holder.cTitle.setText(results.getVolume().getName()+ "\n\n");
+        holder.cTitle.setText(results.getVolume().getName() + "\n\n");
         holder.iNumber.setText(results.getIssue_number());
         Glide.with(context).load(results.getImage().getMedium_url()).into(holder.cImage);
 
@@ -48,13 +49,14 @@ public class Main_Issues_RecyclerAdapter extends RecyclerView.Adapter<Main_Issue
             @Override
             public void onClick(View v) {
 
-                Intent intent=new Intent(context,DetailActivity.class);
-                intent.putExtra("ID","4000-"+results.getId());
-                intent.putExtra("BY","id");
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("ID", "4000-" + results.getId());
+                intent.putExtra("BY", "id");
                 context.startActivity(intent);
             }
         });
     }
+
     @Override
     public int getItemCount() {
         return 25;
@@ -62,15 +64,15 @@ public class Main_Issues_RecyclerAdapter extends RecyclerView.Adapter<Main_Issue
 
     public class MyHolder extends RecyclerView.ViewHolder {
 
-        TextView cTitle,iNumber;
+        TextView cTitle, iNumber;
         ImageView cImage;
 
         public MyHolder(@NonNull View itemView) {
             super(itemView);
 
-            iNumber=itemView.findViewById(R.id.issues_number);
-            cImage=itemView.findViewById(R.id.comics_image);
-            cTitle=itemView.findViewById(R.id.comics_title);
+            iNumber = itemView.findViewById(R.id.issues_number);
+            cImage = itemView.findViewById(R.id.comics_image);
+            cTitle = itemView.findViewById(R.id.comics_title);
         }
     }
 }
